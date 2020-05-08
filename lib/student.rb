8d7f857f304f9bf9a41895f97c467f7c72f4380e
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-05-08 20:34:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,8 +22,9 @@ class Student
 
 
   def self.create_from_collection(students_array)
-    students_array.each do |key, value|
-      self.send
+    students_array.each do |student_hash|
+      self.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
